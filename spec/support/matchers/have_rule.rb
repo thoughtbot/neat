069037b6b07c876801b51a99dccdac5f3bfd922a
Reda Lemeden <reda@thoughtbot.com>
2013-01-24 15:56:54 +0000

    Change interpolation syntax and use map

--- spec/support/matchers/have_rule.rb
+++ spec/support/matchers/have_rule.rb
@@ -1,14 +1,14 @@
 RSpec::Matchers.define :have_rule do |expected|
   match do |actual|
-    @rules = get_rules(actual)? get_rules(actual) : []
+    @rules = get_rules(actual) || []
     @rules.include? expected
   end
 
   failure_message_for_should do |actual|
     if @rules.empty?
-      "selector #{actual} not found"
+      %{selector #{actual} not found}
     else
-      "expected #{actual} to have rule \"#{expected}\""
+      %{expected #{actual} to have rule "#{expected}"}
     end
   end
 
@@ -16,10 +16,9 @@ RSpec::Matchers.define :have_rule do |expected|
     style_block = $parser.find_by_selector(actual)
     unless style_block.empty?
       rules = style_block[0].split(';')
-      rules.each do |rule|
-        rule.gsub!(/^(\s)|(\s)$/, "")
+      rules.map do |rule|
+        rule.gsub(/^(\s)|(\s)$/, "")
       end
-      rules
     end
   end
 end
