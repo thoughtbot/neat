d37233edad2833b9eabae06365be6180aec9c22b
Reda Lemeden <reda@thoughtbot.com>
2013-01-28 11:05:18 +0000

    Update message output

--- spec/support/matchers/have_rule.rb
+++ spec/support/matchers/have_rule.rb
@@ -6,9 +6,9 @@ RSpec::Matchers.define :have_rule do |expected|
 
   failure_message_for_should do |actual|
     if @rules.empty?
-      %{selector #{actual} not found}
+      %{no CSS rules for selector #{actual} were found}
     else
-      %{expected #{actual} to have rule "#{expected}"}
+      %{expected #{actual} to have CSS rule "#{expected}"}
     end
   end
 
