7eeeec3edfc8a050a6094bc76dbfd20698166b00
Reda Lemeden <reda@thoughtbot.com>
2014-02-07 16:35:50 +0100

    Remove table display from omega

--- spec/neat/omega_spec.rb
+++ spec/neat/omega_spec.rb
@@ -11,12 +11,6 @@ describe "@include omega()" do
     end
   end
 
-  context "with argument (table)" do
-    it "removes right padding" do
-      expect('.omega-table').to have_rule('padding-right: 0')
-    end
-  end
-
   context "with argument (4n)" do
     it "removes right margin of nth-child(4n)" do
       expect('.omega-nth-default:nth-child(4n)').to have_rule('margin-right: 0')
@@ -24,12 +18,6 @@ describe "@include omega()" do
 
     it "adds clear to nth-child(4n+1)" do
       expect('.omega-nth-default:nth-child(4n+1)').to have_rule('clear: left')
-    end
-  end
-
-  context "with argument (4n table)" do
-    it "removes right padding of nth-child(4n)" do
-      expect('.omega-nth-table:nth-child(4n)').to have_rule('padding-right: 0')
     end
   end
 
@@ -46,21 +34,9 @@ describe "@include omega()" do
       end
     end
 
-    context "with argument (table)" do
-      it "removes left padding" do
-        expect('section .omega-table-left').to have_rule('padding-left: 0')
-      end
-    end
-
     context "with argument (4n block)" do
       it "removes left margin of nth-child(4n)" do
         expect('section .omega-nth-default-left:nth-child(4n)').to have_rule('margin-left: 0')
-      end
-    end
-
-    context "with argument (4n table)" do
-      it "removes left padding of nth-child(4n)" do
-        expect('section .omega-nth-table-left:nth-child(4n)').to have_rule('padding-left: 0')
       end
     end
   end
