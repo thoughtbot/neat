410fd9614680d48ce33d8ca3f5b229eb1ff9600a
Reda Lemeden <reda@thoughtbot.com>
2014-02-10 15:47:15 +0100

    Remove comments and add specs
    - Closes #140

--- spec/neat/media_spec.rb
+++ spec/neat/media_spec.rb
@@ -42,4 +42,14 @@ describe "@include media()" do
       expect('.change-media-context-shorthand').to have_rule('width: 100%')
     end
   end
+
+  context "with composite argument (max-width 768px min-width 110px orientation portait 6)" do
+    it "outputs @media screen and (max-width: 768px) and (min-width: 110px) and (orientation: portait)" do
+      expect('.composite-media-query').to be_contained_in('screen and (max-width: 768px) and (min-width: 110px) and (orientation: portait)')
+    end
+
+    it "uses a 6-column grid" do
+      expect('.composite-media-query').to have_rule('width: 100%')
+    end
+  end
 end
