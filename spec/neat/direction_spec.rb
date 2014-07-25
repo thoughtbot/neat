25496b1cb5a2ac2d76ec00cd4a623826fefea6e4
Reda Lemeden <reda@thoughtbot.com>
2014-07-25 17:04:57 +0000

    Add direction mixin
    - Closes #200

---
+++ spec/neat/direction_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe "@include direction() {...}" do
+  before(:all) do
+    ParserSupport.parse_file("direction")
+  end
+
+  context "with no argument" do
+    it "uses default direction setting" do
+      expect('.default-block').to have_rule('float: left')
+    end
+  end
+
+  context "whith argument (right-to-left)" do
+    it "changes direction setting inside the block" do
+      expect('.right-to-left-block').to have_rule('float: right')
+    end
+  end
+end
