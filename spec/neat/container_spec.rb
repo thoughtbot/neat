af4c2fe5e166d317e5401c028bb93ad4eff09f51
Reda Lemeden <reda@thoughtbot.com>
2014-08-01 12:22:02 +0100

    Set outer-container max-width with argument
    Closes #201

--- spec/neat/container_spec.rb
+++ spec/neat/container_spec.rb
@@ -12,4 +12,10 @@ describe "@include outer-container()" do
   it "sets max-width" do
     expect('.container-default').to have_rule('max-width: 960px')
   end
+
+  context "with max-width argument: 100%" do
+    it "sets max-width to 100%" do
+      expect('.container-custom-width').to have_rule('max-width: 100%')
+    end
+  end
 end
