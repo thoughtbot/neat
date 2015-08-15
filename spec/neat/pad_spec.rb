fda2414ab4a94e1a4d36b4fcc27b8c4e6db8db20
Mehdi Lahmam <mehdi@craftsmen.io>
2015-08-16 00:22:15 +0200

    Let `pad` mixin accept `null` to skip a side
    Because this mixin was only outputting `padding` property, it was hard
    to avoid overriding inherited padding values.
    Now, we can skip any side by passing `null` in the list of values.
    `pad` now requires Bourbon's `directional-property` mixin.
    
    Fix #113

--- spec/neat/pad_spec.rb
+++ spec/neat/pad_spec.rb
@@ -25,7 +25,29 @@ describe "@include pad()" do
 
   context "with argument (default)" do
     it "uses default gutter percentage" do
-      expect(".pad-shorthand-default").to have_rule("padding: 30px 2.35765% 10px 2.35765%")
+      expect(".pad-shorthand-default").to have_rule("padding: 30px 2.35765% 10px")
+    end
+  end
+
+  context "with argument (null)" do
+    it "ignores rules with null values" do
+      ruleset = "padding-right: 10px; " +
+                "padding-left: 10px;"
+      bad_rule = "padding-top: null;"
+
+      expect(".pad-null-values").to have_ruleset(ruleset)
+      expect(".pad-null-values").to_not have_rule(bad_rule)
+    end
+  end
+
+  context "with argument (null default)" do
+    it "sets right and left paddings to gutter percentage" do
+      ruleset = "padding-right: 2.35765%; " +
+                "padding-left: 2.35765%;"
+      bad_rule = "padding-top: null;"
+
+      expect(".pad-null-and-default").to have_ruleset(ruleset)
+      expect(".pad-null-and-default").to_not have_rule(bad_rule)
     end
   end
 end
