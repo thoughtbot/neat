164d908ba695bba4b7cbc394a12441ed4ced2542
Tyson Gach <tyson@thoughtbot.com>
2016-01-24 11:47:12 -0500

    Remove all tests

--- spec/support/parser_support.rb
+++
@@ -1,9 +0,0 @@
-module ParserSupport
-  def self.parser
-    @parser ||= CssParser::Parser.new
-  end
-
-  def self.parse_file(identifier)
-    self.parser.load_file!("tmp/#{identifier}.css")
-  end
-end
