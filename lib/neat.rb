1f1b8a70d537a54a98fd07b07b42af841d7b5b3c
Tyson Gach <tyson@thoughtbot.com>
2016-02-28 11:22:53 -0500

    Remove engine, Railtie, and install task
    This is an artifact left from the days before the Rails Assets Pipeline. Moving
    forward, this option of install will no longer be available and we'll only
    support Rails 4.2+.

--- lib/neat.rb
+++ lib/neat.rb
@@ -1,19 +1,3 @@
 require "neat/generator"
-
-module Neat
-  if defined?(Rails) && defined?(Rails::Engine)
-    class Engine < ::Rails::Engine
-      require 'neat/engine'
-    end
-
-    module Rails
-      class Railtie < ::Rails::Railtie
-        rake_tasks do
-          load "tasks/install.rake"
-        end
-      end
-    end
-  else
-    Sass.load_paths << File.expand_path("../../core", __FILE__)
-  end
-end
+neat_path = File.expand_path("../../core", __FILE__)
+ENV["SASS_PATH"] = File.join([ENV["SASS_PATH"], neat_path].compact)
