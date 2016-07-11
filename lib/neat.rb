4af34f2dfe1f23bcb2ae737fdd137765e6eab96c
Tyson Gach <tyson@thoughtbot.com>
2016-07-11 09:59:46 -0400

    Fix SASS_PATH
    History:
    
    - We tried simplifying this in Bourbon and then also brought that change
    here to Neat: https://github.com/thoughtbot/bourbon/commit/73b09f618bd3189bf5af6f639e3df07680ed4fc7
    - It then broke the world: https://github.com/thoughtbot/bourbon/pull/879
    - We then went back: https://github.com/thoughtbot/bourbon/commit/a462ed2ac92db0e40ea2a7716eb7f5628e3ea523
    
    This brings the fix update that we put in Bourbon here to Neat.

--- lib/neat.rb
+++ lib/neat.rb
@@ -1,4 +1,7 @@
 require "neat/generator"
 
 neat_path = File.expand_path("../../core", __FILE__)
-ENV["SASS_PATH"] = File.join([ENV["SASS_PATH"], neat_path].compact)
+ENV["SASS_PATH"] = [
+  ENV["SASS_PATH"],
+  neat_path,
+].compact.join(File::PATH_SEPARATOR)
