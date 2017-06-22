c673fd8ca886bdd139e884687cf95b1b7f8f018d
Mehdi Lahmam <mehdi@lahmam.com>
2017-06-22 12:51:49 +0200

    Add support for `--path` option to install
    This allows installing Neat into a specific directory.
    It's more consistent with Bourbon CLI
    
    Fix #351

--- lib/neat/generator.rb
+++ lib/neat/generator.rb
@@ -7,16 +7,18 @@ module Neat
     map ["-v", "--version"] => :version
 
     desc "install", "Install Neat into your project"
+    method_options path: :string, force: :boolean
     def install
-      if neat_files_already_exist?
+      if neat_files_already_exist? && !options[:force]
         puts "Neat files already installed, doing nothing."
       else
         install_files
-        puts "Neat files installed to neat/"
+        puts "Neat files installed to #{install_path}/"
       end
     end
 
     desc "update", "Update Neat"
+    method_options path: :string
     def update
       if neat_files_already_exist?
         remove_neat_directory
@@ -28,6 +30,7 @@ module Neat
     end
 
     desc "remove", "Remove Neat"
+    method_options path: :string
     def remove
       if neat_files_already_exist?
         remove_neat_directory
@@ -45,16 +48,32 @@ module Neat
     private
 
     def neat_files_already_exist?
-      File.directory?("neat")
+      install_path.exist?
     end
 
-    def install_files
-      FileUtils.mkdir_p("neat")
-      FileUtils.cp_r(all_stylesheets, "neat/")
+    def install_path
+      @install_path ||= if options[:path]
+          Pathname.new(File.join(options[:path], "neat"))
+        else
+          Pathname.new("neat")
+        end
     end
 
     def remove_neat_directory
-      FileUtils.rm_rf("neat")
+      FileUtils.rm_rf(install_path)
+    end
+
+    def install_files
+      make_install_directory
+      copy_in_scss_files
+    end
+
+    def make_install_directory
+      FileUtils.mkdir_p(install_path)
+    end
+
+    def copy_in_scss_files
+      FileUtils.cp_r(all_stylesheets, install_path)
     end
 
     def all_stylesheets
