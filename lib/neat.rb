a092ff79a5f81be5a01328f9daa5e15f97b4a58a
Daniel Colson <danieljamescolson@gmail.com>
2018-11-09 14:29:05 -0500

    Register neat with config.assets when in Rails
    Closes #659
    
    I didn't realize this PR had already been opened by another Daniel.
    I changed a few little things, so I will merge this one instead with
    a co-author.
    
    Along the same lines as bourbon 8257c683f8, using `Sass.load_paths`
    won't work if we happen to be using sassc-rails instead of sass-rails.
    I ran into this when I was testing out
    https://github.com/thoughtbot/suspenders/pull/936.
    
    Co-authored-by: Daniel Greenlaw <daniel@shirtspace.com>

--- lib/neat.rb
+++ lib/neat.rb
@@ -1,4 +1,12 @@
 require "sass"
 require "neat/generator"
 
-Sass.load_paths << File.expand_path("../../core", __FILE__)
+module Neat
+  if defined?(Rails::Engine)
+    class Engine < ::Rails::Engine
+      config.assets.paths << File.expand_path("../core", __dir__)
+    end
+  else
+    Sass.load_paths << File.expand_path("../core", __dir__)
+  end
+end
